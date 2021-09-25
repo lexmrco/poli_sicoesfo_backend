42471c2dec051f917aa42523cb8cc75dd14085dd
lexmrco <lexmrco@hotmail.com>
2021-09-25 14:35:11 -0500

    Version inicial

---
+++ poli.sicoesfo/poli.sicoesfo.Domain/Entities/DatoForense.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poli.sicoesfo.Domain.Entities
+{
+    public class DatoForense
+    {
+        public Guid Id { get; set; }
+        public DateTime FechaMuerte { get; set; }
+        public DateTime HoraMuerte { get; set; }
+    }
+}
